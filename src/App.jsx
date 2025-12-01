33f0864201944214f1f78f5610152fa56d8c677b
s2500258 <akulov.alex.wrk@gmail.com>
2025-12-01 23:35:58 +0200

    2025-12-01

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,40 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 
+import { _get, _post, _put } from './hooks/useAxios';
+
 function App() {
 
 const copyright="Copyrights by WP25K";
 const [employees, setEmployees] = useState ([]);
 const [originalEmployees, setOriginalEmployees] = useState([]);
 
+// useEffect(() => {
+//     axios.get("http://localhost:3001/employees")
+//       .then((response) => {
+//         const updatedList = response.data.map(employee => ({
+//             ...employee,
+//             isEditing: false
+//           }));
+//         setEmployees(updatedList);
+//         setOriginalEmployees(updatedList);  
+//       });
+//   }, []);
+
+const fetchData = async () => {
+    try {
+      const response = await _get("/employees", {headers: {}});
+      setEmployees(response.data);
+      setOriginalEmployees(response.data);
+    } catch(error) {
+      console.log("Error fetching data", error)
+    }
+};
+
 useEffect(() => {
-    axios.get("http://localhost:3001/employees")
-      .then((response) => {
-        const updatedList = response.data.map(employee => ({
-            ...employee,
-            isEditing: false
-          }));
-        setEmployees(updatedList);
-        setOriginalEmployees(updatedList);  
-      });
-  }, []);
+  fetchData()
+},[]);
+
 
 const [formData, setFormData] = useState({
     "name": "",
@@ -40,8 +57,26 @@ const [formData, setFormData] = useState({
     "skills": ""
   });
 
-  const handleClick = () => {
-    axios.post("http://localhost:3001/employees", {
+  // const handleClick = () => {
+  //   axios.post("http://localhost:3001/employees", {
+  //     id: String(employees.length+1),
+  //     name: formData.name,
+  //     title: formData.title,
+  //     salary: formData.salary,
+  //     phone: formData.phone,
+  //     email: formData.email,
+  //     animal: formData.animal,
+  //     startDate: formData.startDate,
+  //     location: formData.location,
+  //     department: formData.department,
+  //     skills: formData.skills.split(",")
+  //   }).then((response) => {setEmployees([...employees,response.data])
+  //   });
+  // };
+
+const handleCreate = async () => {
+  try {
+    const newEmployee = {
       id: String(employees.length+1),
       name: formData.name,
       title: formData.title,
@@ -53,9 +88,14 @@ const [formData, setFormData] = useState({
       location: formData.location,
       department: formData.department,
       skills: formData.skills.split(",")
-    }).then((response) => {setEmployees([...employees,response.data])
-    })
-  };
+    };
+    const response = await _post("http://localhost:3001/employees", newEmployee);
+    setEmployees(prev => [...prev, newEmployee]);
+    setOriginalEmployees(prev => [...prev, newEmployee]);
+  } catch (error) {
+    console.error("POST failed:", error);
+  }
+};
 
   const handleEditButton = (id) => {
       setEmployees(employees.map(employee => 
@@ -84,6 +124,35 @@ const [formData, setFormData] = useState({
     );
   };
 
+const handleSaveEmployee = async (id) => {
+  try {
+    const employee = employees.find(emp => emp.id === id);
+    const response = await _put(`http://localhost:3001/employees/${id}`, employee);
+
+    // The updated employee from API
+    const updatedEmployee = response.data;
+
+    // Update Employees
+    setEmployees(prev =>
+      prev.map(emp =>
+        emp.id === id ? { ...emp, isEditing: false } : emp
+      )
+    );
+
+    // Update OriginalEmployees
+    setOriginalEmployees(prev =>
+      prev.map(emp =>
+        emp.id === id ? { ...employee }  : emp
+      )
+    );
+
+    showToast("Employee saved!");
+
+  } catch (err) {
+    console.error("Error updating employee", err);
+  }
+};
+
   const handleSave = (id) => {
     const employee = employees.find(emp => emp.id === id);
     axios.put(`http://localhost:3001/employees/${id}`, {
@@ -100,20 +169,17 @@ const [formData, setFormData] = useState({
       skills: employee.skills
     })
       .then(() => {
-    // update local state to exit editing mode
-        setEmployees(prev =>
+    setEmployees(prev =>
         prev.map(emp =>
         emp.id === id
           ? { ...emp, isEditing: false }
           : emp
       )
     );
-
-    // also update the original backup
     setOriginalEmployees(prev =>
       prev.map(emp =>
         emp.id === id
-          ? { ...employee }  // new saved version becomes original
+          ? { ...employee }  
           : emp
       )
     );
@@ -137,9 +203,9 @@ const showToast = (msg) => {
       {toast && ( <div className="toast">{toast}</div>)}
       <Routes>
         <Route path="/" element={<PersonList employees={employees} handleEditButton={handleEditButton} 
-          handleFieldChange={handleFieldChange} handleCancel={handleCancel} handleSave={handleSave} />} />
+          handleFieldChange={handleFieldChange} handleCancel={handleCancel} handleSave={handleSaveEmployee} />} />
         <Route path="/about" element={<About />} />
-        <Route path="/addemployee" element={<AddEmployee formData={formData} setFormData={setFormData} handleClick={handleClick} />} />
+        <Route path="/addemployee" element={<AddEmployee formData={formData} setFormData={setFormData} handleClick={handleCreate} />} />
       </Routes>
       <Footer copyright={copyright} />
     </Router>
